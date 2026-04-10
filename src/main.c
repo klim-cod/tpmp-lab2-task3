cd64462ae005f4a1fa5593cb7337ae091703c3cd
klim_bliznik <klimbliznik@gmail.com>
2026-04-10 20:13:09 +0300

    Update main.c

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include "client.h"
+#include "server.h"
+
+#define N 10
+
+int main() {
+    GROUP gr[N];
+
+    input_groups(gr, N);
+
+    sort_by_ses(gr, N);
+    print_groups(gr, N);
+
+    printf("\nStudents age <= 20 (01.12.2010):\n");
+    print_young(gr, N);
+
+    return 0;
+}
