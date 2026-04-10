a3056e85219204cc2515da9e270c85ee331c738a
klim_bliznik <klimbliznik@gmail.com>
2026-04-10 20:12:28 +0300

    Update server.h

---
+++ src/server.h
@@ -0,0 +1,10 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include "data.h"
+
+void sort_by_ses(GROUP gr[], int n);
+void print_groups(GROUP gr[], int n);
+void print_young(GROUP gr[], int n);
+
+#endif
